939e428d3cb14ac96379890948c281dcd7828e42
Roméo Froger <romeo.froger@gmail.com>
2025-12-02 14:58:42 +0100

    exercise 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,8 @@
-import requests, json
+"""
+    Import necessary libraries
+"""
+import json
+import requests
 
 def predict_emotion(text_to_analyse):
     """
@@ -9,8 +13,33 @@ def predict_emotion(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     payload = { "raw_document": { "text": text_to_analyse } }
 
-    response = requests.post(api_url, json=payload, headers=header)
+    response = requests.post(api_url, json=payload, headers=header, timeout=10)
 
     formatted_response = json.loads(response.text)
 
-    return formatted_response
+    # Exctract of all the emotion score
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+    # determine the dominant emotion
+    dominate_emotion = max(
+        ['anger', 'disgust', 'fear', 'joy', 'sadness'],
+        key=lambda k: {'anger': anger_score,
+                       'disgust': disgust_score,
+                       'fear': fear_score,
+                       'joy': joy_score,
+                       'sadness': sadness_score}[k]
+    )
+
+    # return the correct output format
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominate_emotion': dominate_emotion    
+    }
